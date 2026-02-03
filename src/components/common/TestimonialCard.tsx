7491000ce81b687bcc7f8f7423ce33c9cf0ad84d
unknown <christyancha@gmail.com>
2026-02-03 15:59:33 +0800

    user adjustment

--- src/components/common/TestimonialCard.tsx
+++ src/components/common/TestimonialCard.tsx
@@ -1,66 +1,40 @@
 import './TestimonialCard.css'
 
 interface TestimonialCardProps {
-  quoteIcon: string
   avatar: string
   text: string
   name: string
   role: string
-  secondName?: string
-  secondRole?: string
-  decorativeVector: string
-  style: 'style-1' | 'style-2' | 'style-3'
 }
 
 function TestimonialCard({
-  quoteIcon,
   avatar,
   text,
   name,
   role,
-  secondName,
-  secondRole,
-  decorativeVector,
-  style,
 }: TestimonialCardProps) {
   return (
-    <article className={`testimonial-card testimonial-card-${style}`}>
-      <img
-        src={quoteIcon}
-        alt="Quote icon"
-        className="testimonial-quote-icon"
-      />
-      <div className="testimonial-content-wrapper">
-        <p className="testimonial-text">{text}</p>
-      </div>
+    <article className="testimonial-card">
       <img
         src={avatar}
         alt={name}
         className="testimonial-avatar"
       />
-      <div className="testimonial-bottom-section">
-        <img
-          src={decorativeVector}
-          alt="Decorative vector"
-          className="testimonial-decorative-vector"
-        />
-        {style === 'style-1' && secondName ? (
-          <div className="testimonial-names-group">
-            <div className="testimonial-name-group">
-              <h3 className="testimonial-name">{name}</h3>
-              <p className="testimonial-role">{role}</p>
-            </div>
-            <div className="testimonial-name-group">
-              <h3 className="testimonial-name">{secondName}</h3>
-              <p className="testimonial-role">{secondRole}</p>
-            </div>
+      <div className="testimonial-content">
+        <div className="testimonial-quote-text-wrapper">
+          <div className="testimonial-quote-icon-small">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="12" cy="12" r="12" fill="#4A90E2"/>
+              <path d="M7.5 10.5C7.5 9 9 7.5 10.5 7.5C12 7.5 13.5 9 13.5 10.5C13.5 12 12 13.5 10.5 13.5C9.75 13.5 9 13.2 8.4 12.9V15H7.5V10.5Z" fill="white"/>
+              <path d="M16.5 10.5C16.5 9 18 7.5 19.5 7.5C21 7.5 22.5 9 22.5 10.5C22.5 12 21 13.5 19.5 13.5C18.75 13.5 18 13.2 17.4 12.9V15H16.5V10.5Z" fill="white"/>
+            </svg>
           </div>
-        ) : (
-          <div className="testimonial-name-group">
-            <h3 className="testimonial-name">{name}</h3>
-            <p className="testimonial-role">{role}</p>
-          </div>
-        )}
+          <p className="testimonial-text">{text}</p>
+        </div>
+        <div className="testimonial-author">
+          <h3 className="testimonial-name">{name}</h3>
+          <p className="testimonial-role">{role}</p>
+        </div>
       </div>
     </article>
   )
